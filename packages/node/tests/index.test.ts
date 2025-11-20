961c3baa128dae8294d9c8315a78d2b07c382dca
ThatXliner <bryan.hu.2020@gmail.com>
2025-11-19 20:24:42 -0800

    test fixed

--- packages/node/tests/index.test.ts
+++ packages/node/tests/index.test.ts
@@ -1,7 +1,6 @@
 import { test, expect, describe } from 'bun:test';
 import { extractMetadata } from '../src/index';
-import { ZU, Item } from 'ztractor';
-import { parseTranslatorMetadata } from '../src/translator-loader';
+import { ZU, Item, parseTranslatorMetadata } from 'ztractor';
 
 describe('Zotero Utilities', () => {
   test('trimInternal removes extra whitespace', () => {
